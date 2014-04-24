5a6e99dd6b3e6f24cf37751bdc44117905e6f078
SkippyMiller <millerm68@gmail.com>
2014-04-24 16:21:41 -0500

    First attempt to upload to git

---
+++ com/VandalayIndustries/employeedirectory/Search.java
@@ -0,0 +1,15 @@
+package com.VandalayIndustries.employeedirectory;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+
+@SuppressLint("Registered")
+public class Search extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
